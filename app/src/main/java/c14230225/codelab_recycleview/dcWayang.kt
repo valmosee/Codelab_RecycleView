68fa10b2f6446348be2ef8dd3cd9727119d24fe3
Joice <c14230225@john.petra.ac.id>
2025-11-04 19:39:45 +0700

    47 - 55

--- app/src/main/java/c14230225/codelab_recycleview/dcWayang.kt
+++ app/src/main/java/c14230225/codelab_recycleview/dcWayang.kt
@@ -1,8 +1,12 @@
 package c14230225.codelab_recycleview
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class dcWayang(
     var foto : String,
     var nama : String,
     var karakter : String,
     var deskripsi : String
-)
+) : Parcelable
---
+++ app/src/main/java/c14230225/codelab_recycleview/detWayang.kt
@@ -0,0 +1,20 @@
+package c14230225.codelab_recycleview
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class detWayang : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_det_wayang)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
